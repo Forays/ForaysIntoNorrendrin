19b53210ef6acc9e30becbe9feb99d7111b86fbe
derrickcreamer <derrickcreamer@gmail.com>
2015-08-12 17:23:43 -0400

    Use NuGet for OpenTK. Add project file for building terminal version.

--- Forays/Main.cs
+++ Forays/Main.cs
@@ -1,4 +1,4 @@
-/*Copyright (c) 2011-2014  Derrick Creamer
+/*Copyright (c) 2011-2015  Derrick Creamer
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
 files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish,
 distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -63,13 +63,13 @@ namespace Forays{
 		}
 		
 		static void Main(string[] args){
+			//Screen.GLMode = false;
 			{
 				int os = (int)Environment.OSVersion.Platform;
 				if(os == 4 || os == 6 ||  os == 128){
 					Global.LINUX = true;
 				}
 			}
-			Screen.GLMode = false;
 			if(args != null && args.Length > 0){
 				if(args[0] == "-c" || args[0] == "--console"){
 					Screen.GLMode = false;
--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-ForaysIntoNorrendrin
-====================
+Forays into Norrendrin
+======================
 
 A streamlined roguelike with deeply tactical combat.
+
+<br>
+
+[Read more about the game here.](http://forays.github.io/)
+
+<br>
+
+#### How to build the console version:
+
+By default, the project will run in its own window, using
+OpenTK. To make it run in a terminal, follow these steps:
+
+1. Open ConsoleForays.csproj instead of opening the main
+   Forays.sln or Forays.csproj, *or*, temporarily replace
+   Forays.csproj with ConsoleForays.csproj and then open
+   the main Forays.sln solution.
+2. Change the first line of the Main method (in Main.cs)
+   by uncommenting "Screen.GLMode = false;".
+3. Compile!
+
